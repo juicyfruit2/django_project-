69c1d66e4cbe823da6cc38453338958344f43030
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-06-08 17:35:51 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,10 @@
 import os 
 import sys
 sys.path.insert(0,os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Your_project_name.settings'
+django.setup()
+
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -22,8 +26,6 @@ extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
